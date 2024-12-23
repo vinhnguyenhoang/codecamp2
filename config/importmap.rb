c620393c4eb54553cea8538b4aa417d2456718b1
vinhnh0004 <vinhnh@zigexn.vn>
2024-12-23 16:11:00 +0700

    init project

---
+++ config/importmap.rb
@@ -0,0 +1,7 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
